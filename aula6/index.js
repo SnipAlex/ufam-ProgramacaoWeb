f525b3fb481458158136f449a81d9cd7c3017ac5
Alexandre da Fonseca Vieira <107149857+SnipAlex@users.noreply.github.com>
2022-11-23 17:28:21 -0400

    Aula 6
    aula 6 sobre js e suas características

---
+++ aula6/index.js
@@ -0,0 +1,10 @@
+let vetor = [ 2, 4, 6, 7, 8]
+const v2 = vetor.map((v) => v%2 === 1)
+
+console.log(v2)
+
+const multiplica = (a, b) => a * b // mesma coisa 
+
+function multip (a, b) { // que isso aqui
+    return a * b
+}
---
+++ aula6/script.js
@@ -0,0 +1,8 @@
+alert("Seja bem vindo") // Isso é um comentário Javascript
+/**
+ * Isso     
+ * também
+ * é
+ * um
+ * comentário
+*/
