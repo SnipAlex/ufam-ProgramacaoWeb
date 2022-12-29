edb1cca76a3050767c3093b102efc893f2f9f15c
Alexandre da Fonseca Vieira <107149857+SnipAlex@users.noreply.github.com>
2022-12-28 20:02:11 -0400

    Mudanças no JS5

---
+++ tarefas/JS5/main.js
@@ -0,0 +1,9 @@
+function DuasCoisas()
+{
+    let raio = document.querySelector('#num').value
+    
+
+    console.log("Area " + (3.14 * (raio*raio)).toFixed(2))
+    console.log("Circunferencia " + (2*3.14*raio).toFixed(2))
+
+}
