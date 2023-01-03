50b03724245a660e98a81029baaa81f91fc29b0e
Alexandre da Fonseca Vieira <107149857+SnipAlex@users.noreply.github.com>
2023-01-03 09:07:05 -0400

    Implementação inicial do jogo

---
+++ tarefas/JS6/script.js
@@ -0,0 +1,14 @@
+function grafico()
+{
+    let lagura = document.getElementById('l1').value;
+    let altura1 = document.getElementById('b1').value;
+    let altura2 = document.getElementById('b2').value;
+    let altura3 = document.getElementById('b3').value;
+    let altura4 = document.getElementById('b4').value;
+    let altura5 = document.getElementById('b5').value;
+    
+    let qua1 = document.getElementById("q1");
+    qua1.style.removeProperty("height")
+    // qua1.style.setProperty("height", altura1);
+    
+}
