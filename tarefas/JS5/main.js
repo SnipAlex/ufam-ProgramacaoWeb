95d1db3154d4039a02cd8a9db47efee823967d93
Alexandre F <alexandre36viieira@gmail.com>
2023-01-09 22:13:10 -0400

    Add npm and start a express project

--- tarefas/JS5/main.js
+++ tarefas/JS5/main.js
@@ -2,12 +2,8 @@ function DuasCoisas()
 {
     let raio = document.querySelector('#num').value
     
-    let areaCalculo = (3.14 * (raio*raio)).toFixed(2)
-    let circunferenciaCalculo = (2*3.14*raio).toFixed(2)
-    
-    console.log("Area " + areaCalculo)
-    console.log("Circunferencia " + circunferenciaCalculo)
-    
-    document.getElementById("area").value = areaCalculo
-    document.getElementById("circunf").value = circunferenciaCalculo
+
+    console.log("Area " + (3.14 * (raio*raio)).toFixed(2))
+    console.log("Circunferencia " + (2*3.14*raio).toFixed(2))
+
 }
