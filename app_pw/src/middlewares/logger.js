d020b42b260baa6b419d9963be29bf5367d7ab6e
Alexandre da Fonseca Vieira <107149857+SnipAlex@users.noreply.github.com>
2023-01-10 10:55:26 -0400

    Revert "Add npm and start a express project"
    This reverts commit 95d1db3154d4039a02cd8a9db47efee823967d93.

--- app_pw/src/middlewares/logger.js
+++
@@ -1,8 +0,0 @@
-function logger() {
-    return (req, res, next) => {
-        console.log("Informações de log")
-        next()
-    }   
-}
-
-module.exports = logger;
--- node1/index.js
+++
@@ -1,6 +0,0 @@
-import http from "http";
-
-http.createServer((req, res) => {
-    console.log(`Counter ${++counter}`);
-    
-}).listen(3000)
