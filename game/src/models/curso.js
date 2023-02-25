b54b81c598d806be767f83d5fdd46d437b8f1f78
Alexandre da Fonseca Vieira <107149857+SnipAlex@users.noreply.github.com>
2023-02-24 20:41:43 -0400

    faltou um kk

--- game/src/models/curso.js
+++ game/src/models/curso.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       this.belongsTo(models.Area);
+      this.hasMany(models.User)
     }
   }
   Curso.init({
